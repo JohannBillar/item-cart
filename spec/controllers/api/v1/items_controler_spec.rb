4023ff4dd3140f9086edf3ed04cb3215944852fa
JohannBillar <jwbillar@gmail.com>
2017-02-11 18:47:59 -0800

    Add Factory Girl gem

--- spec/controllers/api/v1/items_controler_spec.rb
+++ spec/controllers/api/v1/items_controler_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
+
   describe "items#index action" do
     it "should succesfully show the page" do
-      get 'api/v1/items'
-      # json = JSON.parse(respose.body)
+      get :index
       expect(response).to have_http_status(:success)
     end
   end
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :item do
+    name "item name"
+    description "i am a test item"
+  end
+
+end
