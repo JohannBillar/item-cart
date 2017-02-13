a3c04f76b0f104f534409f30b28b0e15c38e2986
JohannBillar <jwbillar@gmail.com>
2017-02-12 17:36:47 -0800

    Change inputs to controlled components

--- app/assets/javascripts/components/_item.js.jsx
+++ app/assets/javascripts/components/_item.js.jsx
@@ -2,16 +2,23 @@ class Item extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { editable: false };
+    this.state = { editable: false, name: this.props.item.name, description: this.props.item.description };
 
     this.handleEdit = this.handleEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(label, e) {
+    this.setState({
+      [label]: e.target.value,
+    });
   }
 
   handleEdit() {
     if (this.state.editable) {
       const id = this.props.item.id;
-      const name = this.refs.name.value;
-      const description = this.refs.description.value;
+      const name = this.state.name;
+      const description = this.state.description;
       const item = { id: id, name: name, description: description };
       this.props.handleUpdate(item);
     }
@@ -19,8 +26,8 @@ class Item extends React.Component {
   }
 
   render() {
-    const name = this.state.editable ? <input type="text" ref="name" defaultValue={this.props.item.name} /> : <h3>{this.props.item.name}</h3>;
-    const description = this.state.editable ? <input type="text" ref="description" defaultValue={this.props.item.description} /> : <p>{this.props.item.description}</p>;
+    const name = this.state.editable ? <input type="text" value={this.state.name}  onChange={e => this.handleChange('name', e)} className="col-4 form-control" /> : <h3>{this.props.item.name}</h3>;
+    const description = this.state.editable ? <input type="text" value={this.state.description} onChange={e => this.handleChange('description', e)} className="col-4 form-control" /> : <p>{this.props.item.description}</p>;
     const buttonStyle = this.state.editable ? 'btn btn-outline-primary btn-sm' : 'btn btn-outline-success btn-sm'
     return (
       <div className="col-12 offset-2 items">
--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -1,27 +1,35 @@
 class NewItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { name: '', description: '' };
     
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(label, e) {
+    this.setState({
+      [label]: e.target.value,
+    });
   }
   
   handleClick() {
-    const name = this.refs.name.value;
-    const description = this.refs.description.value;
     $.ajax({
       url: 'api/v1/items',
       type: 'POST',
-      data: { item: { name: name, description: description }},
+      data: { item: { name: this.state.name, description: this.state.description }},
       success: (item => this.props.handleSubmit(item)),
     });
+    this.setState({ name: '', description: '' });
   }
   
   render() {
     return (
       <div className="col-12 offset-2">
         <div className="row">
-          <input type="text" ref="name" placeholder="Enter the name of an item" className="col-4 form-control" />
-          <input type="text" ref="description" placeholder="Enter a description" className="col-4 form-control" />
+          <input type="text" value={this.state.name} onChange={e => this.handleChange('name', e)} placeholder="Enter the name of an item" className="col-4 form-control" />
+          <input type="text" value={this.state.description} onChange={e => this.handleChange('description', e)}  placeholder="Enter a description" className="col-4 form-control" />
           <button type="button" onClick={this.handleClick} className="btn btn-outline-primary">Submit</button>
         </div>
       </div>
